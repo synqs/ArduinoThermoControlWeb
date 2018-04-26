fac4e058a59fb7a038d0cc398a1d40496d628852
Fred Jendrzejewski <fnj@kip.uni-heidelberg.de>
2018-04-26 12:47:05 +0200

    Try to open a serial port and handle the error.

--- app/forms.py
+++ app/forms.py
@@ -3,9 +3,5 @@ from flask_wtf.file import DataRequired, FileRequired
 from wtforms import StringField, SubmitField, FileField
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    submit = SubmitField('Sign In')
-
-class FileForm(FlaskForm):
-    file = FileField('File:', validators=[FileRequired()])
-    submit = SubmitField('Upload')
+    serial_port = StringField('Serial port of the form COM32', validators=[DataRequired()])
+    submit = SubmitField('Open port')
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from app import app
-from app.forms import LoginForm, FileForm
+from app.forms import LoginForm
+import serial
 from flask import render_template, flash, redirect
 
 @app.route('/')
@@ -7,14 +8,23 @@ from flask import render_template, flash, redirect
 def index():
     form = LoginForm()
 
-    fileform = FileForm()
-    if fileform.validate_on_submit():
-        print('Test test test')
-        f = fileform.file.data
-        print(f)
-        df = data(f)
-        flash('We got the following filename {}'.format(fileform.file.data.filename))
+    if form.validate_on_submit():
+        flash('We got the following port {}'.format(form.serial_port.data))
+        try:
+            ser = serial.Serial('COM32', 9600, timeout = 1)
+        # except serial.SerialException:
+        #     s.close()
+        #     ser.close()
+        #     ser = serial.Serial('COM32', 9600, timeout = 1)
+        except Exception as e:
+             flash('{}'.format(e), 'error')
         return redirect('/index')
 
     lyseout = 'This is some dummy output from lyse.'
-    return render_template('index.html', lyseout=lyseout, form=form, fileform=fileform)
+    return render_template('index.html', lyseout=lyseout, form=form)
+
+
+@app.errorhandler(500)
+def internal_error(error):
+    flash('An error occured {}'.format(error), 'error')
+    return render_template('500.html'), 500
