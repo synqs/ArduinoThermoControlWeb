38e60ac375bd3c6bfeae927d1df323796df4904d
Fred Jendrzejewski <fred.jendrzejewski@gmail.com>
2018-05-11 17:31:01 +0200

    Started to add WebsocketIO.
    This might be a nice way to communicate data to the website.

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,14 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
+from flask_socketio import SocketIO
 from config import Config
+
+# where should I move this normally ?
+async_mode = None
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+socketio = SocketIO(app, async_mode=async_mode)
 
 app.config.from_object(Config)
 
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,9 @@
-from app import app
+from app import app, socketio
 from app.forms import ConnectForm, DataForm
 import serial
 import h5py
-from flask import render_template, flash, redirect, send_file
+from flask import render_template, flash, redirect, send_file, url_for, session
+from flask_socketio import emit, disconnect
 
 # for subplots
 from io import BytesIO
@@ -39,11 +40,11 @@ def index():
         df = df.append(create_test_data(), ignore_index = True);
         flash('We would like to submit some data locally. We have here {}'.format(df))
         flash('We would like to submit some data remote. We have here {}'.format(app.config['REMOTE_FILE']))
-        return redirect('/index')
+        return redirect(url_for('index'))
 
     # this could now actually become the last hundred lines of the dataframe.
     lyseout = 'This is some dummy output from lyse.'
-    return render_template('index.html', lyseout=fname, dform = dform)
+    return render_template('index.html', lyseout=fname, dform = dform, async_mode=socketio.async_mode)
 
 @app.route('/config', methods=['GET', 'POST'])
 def config():
@@ -62,7 +63,7 @@ def config():
         #     ser = serial.Serial('COM32', 9600, timeout = 1)
         except Exception as e:
              flash('{}'.format(e), 'error')
-        return redirect('/index')
+        return redirect(url_for('index'))
 
     return render_template('config.html', port = port, form=form)
 
@@ -104,6 +105,37 @@ def chartData(entries):
     # Return the result with JSON format
     return df.to_json()
 
+# communication with the websocket
+@socketio.on('my_ping', namespace='/test')
+def ping_pong():
+    emit('my_pong')
+
+@socketio.on('my_event', namespace='/test')
+def test_message(message):
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    Vinp = np.random.randint(50);
+    emit('my_response',
+         {'data': Vinp, 'count': session['receive_count']})
+
+@socketio.on('my_broadcast_event', namespace='/test')
+def test_message(message):
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my_response',
+         {'data': message['data'], 'count': session['receive_count']},
+         broadcast=True)
+
+@socketio.on('connect', namespace='/test')
+def test_connect():
+    emit('my response', {'data': 'Connected'})
+
+@socketio.on('disconnect_request', namespace='/test')
+def test_disconnect():
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my_response',
+         {'data': 'Disconnected!', 'count': session['receive_count']})
+    disconnect()
+
+# error handling
 @app.errorhandler(500)
 def internal_error(error):
     flash('An error occured {}'.format(error), 'error')
