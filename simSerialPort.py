845a989ecb2ac45fc2ca873ee42dcfe7905abb9b
Fred Jendrzejewski <fred.jendrzejewski@gmail.com>
2018-05-25 09:44:02 +0200

    Created the more costum log_response
    Basically I want to simplify the read-out. Started to do this by sending the arduino data already through a list. Next step is to create a table.

--- simSerialPort.py
+++ simSerialPort.py
@@ -1,4 +1,19 @@
 import os, pty
-master, slave = pty.openpty()
-s_name = os.ttyname(slave)
-print(s_name)
+import time
+import numpy as np
+
+def test_serial():
+    master, slave = pty.openpty()
+    s_name = os.ttyname(slave)
+    print(s_name)
+    while True:
+        err = np.random.randint(5)
+        meas = np.random.randint(200)
+        control = np.random.randint(100)
+        ard_str = str(err) + ',' + str(meas) + ',' + str(control) + '\r\n'
+        b = ard_str.encode('windows-1252')
+        os.write(master, b)
+        print('Wrote to master')
+        time.sleep(1)
+if __name__=='__main__':
+    test_serial()
