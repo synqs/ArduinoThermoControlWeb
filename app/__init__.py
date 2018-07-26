e66411d36ea727c4d77d5785368fdd068ae78b56
Fred Jendrzejewski <fred.jendrzejewski@gmail.com>
2018-07-26 10:12:24 +0200

    Add the email support
    However, we have to save the login information on the PC. This has to be done properly, without comprimising passwords etc.

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,11 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import eventlet
+
+import logging
+from logging.handlers import SMTPHandler
+
+
 eventlet.monkey_patch()
 
 # where should I move this normally ?
@@ -36,3 +41,19 @@ app.register_blueprint(thermocontrol_bp)
 
 from app.serialmonitor import bp as serialmonitor_bp
 app.register_blueprint(serialmonitor_bp)
+
+if not app.debug:
+    if app.config['MAIL_SERVER']:
+        auth = None
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+        secure = None
+        if app.config['MAIL_USE_TLS']:
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+            fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+            toaddrs=app.config['ADMINS'], subject='Microblog Failure',
+            credentials=auth, secure=secure)
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
