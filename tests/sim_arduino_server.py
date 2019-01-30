8ff8d7ce6e9382cecd4049a8b3e176a851e15225
Fred Jendrzejewski <fred.jendrzejewski@gmail.com>
2019-01-30 17:13:47 +0100

    Update sim_arduino_server.py
    Read out the setpoint of the arduino

--- tests/sim_arduino_server.py
+++ tests/sim_arduino_server.py
@@ -2,11 +2,19 @@
 An extremely simple server that might be used for testing the arduino webserver interface.
 '''
 
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
+
 @app.route('/')
 def home():
-    return "Hey there!"
+    setpoint = 0;
+    
+    for el in request.args:
+        if el == 's':
+            print('Update the setpoint');
+            setpoint = float(request.args[el])
+            print(setpoint)
+    return "setpoint = {} there!".format(setpoint)
 
 if __name__ == '__main__':
     app.run(port=5001, debug=True)
