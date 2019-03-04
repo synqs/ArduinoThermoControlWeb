0e9ca2e1612e341658bd1934ab5ce5e53edf6326
Fred J <fred.jendrzejewski@gmail.com>
2019-03-04 12:06:40 +0100

    Updated the YUn Arduino program

--- ArduinoPrograms/YunPutAndGet/YunPutAndGet.ino
+++ ArduinoPrograms/YunPutAndGet/YunPutAndGet.ino
@@ -142,19 +142,9 @@ void process(BridgeClient client) {
     readCommand(client);
   }
   
-  // is "digital" command?
-  if (command == "digital") {
-    digitalCommand(client);
-  }
-
   // is "analog" command?
-  if (command == "analog") {
+  if (command == "write") {
     analogCommand(client);
-  }
-
-  // is "mode" command?
-  if (command == "mode") {
-    modeCommand(client);
   }
 }
 
@@ -194,105 +184,57 @@ void readCommand(BridgeClient client) {
   Bridge.put(key, String(value));
 }
 
-void digitalCommand(BridgeClient client) {
-  int pin, value;
-
-  // Read pin number
-  pin = client.parseInt();
-
-  // If the next character is a '/' it means we have an URL
-  // with a value like: "/digital/13/1"
-  if (client.read() == '/') {
-    value = client.parseInt();
-    digitalWrite(pin, value);
-  } else {
-    value = digitalRead(pin);
-  }
-
-  // Send feedback to client
-  client.print(F("Pin D"));
-  client.print(pin);
-  client.print(F(" set to "));
-  client.println(value);
-
-  // Update datastore key with the current pin value
-  String key = "D";
-  key += pin;
-  Bridge.put(key, String(value));
-}
-
 void analogCommand(BridgeClient client) {
-  int pin, value;
-
-  // Read pin number
-  pin = client.parseInt();
-
-  // If the next character is a '/' it means we have an URL
-  // with a value like: "/analog/5/120"
-  if (client.read() == '/') {
-    // Read value and execute command
-    value = client.parseInt();
-    analogWrite(pin, value);
-
-    // Send feedback to client
-    client.print(F("Pin D"));
-    client.print(pin);
-    client.print(F(" set to analog "));
-    client.println(value);
-
-    // Update datastore key with the current pin value
-    String key = "D";
-    key += pin;
-    Bridge.put(key, String(value));
-  } else {
-    // Read analog pin
-    value = analogRead(pin);
-
-    // Send feedback to client
-    client.print(F("Pin A"));
-    client.print(pin);
-    client.print(F(" reads analog "));
-    client.println(value);
-
-    // Update datastore key with the current pin value
-    String key = "A";
-    key += pin;
-    Bridge.put(key, String(value));
+  
+  String component = client.readStringUntil('/');
+  if (component == "setpoint") {
+   float value = 0;
+    value = client.parseFloat();
+    if (value){
+      setpoint = value;      
+    }
+    client.println("Change the setpoint"); 
+    client.println(setpoint);
+    String key = "setpoint";
+    Bridge.put(key, String(setpoint));
   }
-}
-
-void modeCommand(BridgeClient client) {
-  int pin;
-
-  // Read pin number
-  pin = client.parseInt();
-
-  // If the next character is not a '/' we have a malformed URL
-  if (client.read() != '/') {
-    client.println(F("error"));
-    return;
+  if (component == "gain") {
+   float value = 0;
+    value = client.parseFloat();
+    if (value){
+      G = value;
+      kp = G;
+      ki = G / tauI;
+      kd = G * tauD;
+    }
+    client.print("Change the gain"); 
+    client.println(G);
+    String key = "gain";
+    Bridge.put(key, String(G));
   }
-
-  String mode = client.readStringUntil('\r');
-
-  if (mode == "input") {
-    pinMode(pin, INPUT);
-    // Send feedback to client
-    client.print(F("Pin D"));
-    client.print(pin);
-    client.print(F(" configured as INPUT!"));
-    return;
+  
+  if (component == "integral") {
+   float value = 0;
+    value = client.parseFloat();
+    if (value){
+      tauI = value;
+      ki = G / tauI;
+    }
+    client.print("Change the integral"); 
+    client.println(tauI);
+    String key = "integral";
+    Bridge.put(key, String(tauI));
   }
-
-  if (mode == "output") {
-    pinMode(pin, OUTPUT);
-    // Send feedback to client
-    client.print(F("Pin D"));
-    client.print(pin);
-    client.print(F(" configured as OUTPUT!"));
-    return;
+  if (component == "differential") {
+   float value = 0;
+    value = client.parseFloat();
+    if (value){
+      tauD = value;
+      kd = G*tauD;
+    }
+    client.println("Change the differential"); 
+    client.println(tauD);
+    String key = "differential";
+    Bridge.put(key, String(tauD));
   }
-
-  client.print(F("error: invalid mode "));
-  client.print(mode);
 }
