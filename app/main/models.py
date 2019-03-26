57f3b4611504bc0216eccd7dd7b619a33e82cf8f
Fred J <fred.jendrzejewski@gmail.com>
2019-03-26 20:50:18 +0100

    Login and Logout is running now.

--- app/main/models.py
+++ app/main/models.py
@@ -1,5 +1,6 @@
 from flask_login import UserMixin
 from app import db, login
+from werkzeug.security import generate_password_hash, check_password_hash
 
 @login.user_loader
 def load_user(id):
@@ -13,3 +14,9 @@ class User(UserMixin, db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
--- app/main/routes.py
+++ app/main/routes.py
@@ -1,7 +1,7 @@
 from app import socketio, db
 from app.main import bp
 
-from app.main.forms import LoginForm
+from app.main.forms import LoginForm, RegistrationForm
 from app.main.models import User
 
 from app.thermocontrol.models import TempControl, WebTempControl
@@ -11,7 +11,7 @@ from app.cameracontrol.models import Camera
 from flask import render_template, flash, redirect, url_for, session
 from flask_socketio import emit, disconnect
 
-from flask_login import current_user, login_user
+from flask_login import current_user, login_user, logout_user
 
 @bp.route('/')
 @bp.route('/index', methods=['GET', 'POST'])
@@ -38,7 +38,7 @@ def index():
 @bp.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(url_for('main.index'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
@@ -48,6 +48,25 @@ def login():
         login_user(user, remember=form.remember_me.data)
         return redirect(url_for('main.index'))
     return render_template('login.html', title='Sign In', form=form)
+
+@bp.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('main.index'))
+
+@bp.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('main.login'))
+    return render_template('register.html', title='Register', form=form)
 
 # communication with the websocket
 @socketio.on('connect')
