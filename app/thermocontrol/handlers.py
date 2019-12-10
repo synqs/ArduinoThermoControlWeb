a9be78d647fa9a0f9e24f65b06d764512aa3799a
Fred J <fred.jendrzejewski@gmail.com>
2019-12-10 12:05:41 +0100

    Clean out old tempcontrol code.

--- app/thermocontrol/handlers.py
+++ app/thermocontrol/handlers.py
@@ -109,17 +109,6 @@ def start_wtc(ard_nr):
     tc = WebTempControl.query.get(ard_nr);
     return start_helper(tc);
 
-@bp.route('/stop/<int:ard_nr>')
-def stop(ard_nr):
-    '''
-    The main function for rendering the principal site.
-    '''
-    tc = TempControl.query.get(ard_nr);
-    tc.stop()
-    flash('Stopped the thermocontrol')
-    socketio.emit('close_conn',{'data': tc.conn_str()})
-    return redirect(url_for('main.index'))
-
 @bp.route('/stop_wtc/<int:ard_nr>')
 @login_required
 def stop_wtc(ard_nr):
@@ -132,46 +121,6 @@ def stop_wtc(ard_nr):
     socketio.emit('close_conn',{'data': tc.conn_str()})
     return redirect(url_for('main.index'))
 
-@bp.route('/change_arduino/<int:ard_nr>')
-def change_arduino(ard_nr):
-    '''
-    Change the parameters of a specific arduino
-    '''
-    arduino = TempControl.query.get(ard_nr);
-    device_type = 'serial_tc';
-
-    if not arduino:
-        flash('No tempcontrols installed', 'error')
-        return redirect(url_for('thermocontrol.add_tempcontrol'));
-
-    uform, sform, gform, iform, diff_form, wform, dform = get_tc_forms(ard_nr);
-
-    return render_template('change_arduino.html',
-        form=uform, dform = dform, sform = sform, gform = gform, iform = iform,
-        diff_form = diff_form, wform = wform, ard=arduino, device_type=device_type);
-
-@bp.route('/change_wtc/<int:ard_nr>')
-@login_required
-def change_wtc(ard_nr):
-    '''
-    Change the parameters of a specific arduino
-    '''
-    arduino = WebTempControl.query.get(ard_nr);
-    device_type = 'web_tc';
-
-    if not arduino:
-        flash('No tempcontrols installed', 'error')
-        return redirect(url_for('thermocontrol.add_webtempcontrol'));
-
-    if not current_user.id == arduino.user_id:
-        flash('Access denied', 'error')
-        return redirect(url_for('main.index'));
-
-    uform, sform, gform, iform, diff_form, wform, dform = get_wtc_forms(ard_nr);
-
-    return render_template('change_arduino.html',
-        form=uform, dform = dform, sform = sform, gform = gform, iform = iform,
-        diff_form = diff_form, wform = wform, ard=arduino, device_type=device_type);
 
 @bp.route('/update_wtc', methods=['POST'])
 @login_required
@@ -197,34 +146,6 @@ def update_wtc():
         return render_template('change_arduino.html', form=uform, dform = dform,
             sform = sform, gform = gform, iform = iform, diff_form = diff_form, wform = wform, ard=arduino);
 
-@bp.route('/serialwait_tc', methods=['POST'])
-def serialwait():
-    '''
-    Update the serial waiting time.
-    '''
-
-    sform = UpdateSetpointForm();
-    uform = UpdateForm();
-    wform = SerialWaitForm()
-    dform = DisconnectForm()
-    gform = UpdateGainForm()
-    iform = UpdateIntegralForm()
-    diff_form = UpdateDifferentialForm()
-
-    id = int(wform.id.data);
-    arduino = TempControl.query.get(id);
-
-    if wform.validate_on_submit():
-        n_wait =  wform.serial_time.data;
-        arduino.sleeptime = n_wait;
-        db.session.commit();
-        flash('We update every {} s'.format(n_wait))
-        return redirect(url_for('thermocontrol.change_arduino', ard_nr = id))
-    else:
-        return render_template('change_arduino.html', form=uform, dform = dform,
-            cform = cform,  sform = sform, gform = gform, iform = iform,
-            diff_form = diff_form, wform = wform, ard=arduino);
-
 @bp.route('/wait_wtc', methods=['POST'])
 @login_required
 def wait_wtc():
@@ -246,35 +167,6 @@ def wait_wtc():
             cform = cform,  sform = sform, gform = gform, iform = iform,
             diff_form = diff_form, wform = wform, ard=arduino);
 
-@bp.route('/setpoint_tc', methods=['POST'])
-def arduino():
-    '''
-    Configure now settings for the arduino.
-    '''
-    sform = UpdateSetpointForm();
-    uform = UpdateForm();
-    wform = SerialWaitForm()
-    dform = DisconnectForm()
-    gform = UpdateGainForm()
-    iform = UpdateIntegralForm()
-    diff_form = UpdateDifferentialForm()
-
-    id = int(sform.id.data);
-    arduino = TempControl.query.get(id);
-
-    if sform.validate_on_submit():
-        arduino.setpoint = sform.setpoint.data;
-        db.session.commit();
-        success = arduino.set_setpoint();
-        if success:
-            flash('We set the setpoint to {}'.format(sform.setpoint.data))
-        else:
-            flash('Serial port not open.', 'error')
-        return redirect(url_for('thermocontrol.change_arduino', ard_nr = id))
-    else:
-        return render_template('change_arduino.html', form=uform, dform = dform,
-            sform = sform, gform = gform, iform = iform,
-            diff_form = diff_form, wform = wform, ard=arduino);
 
 @bp.route('/setpoint_wtc', methods=['POST'])
 @login_required
@@ -299,38 +191,6 @@ def setpoint_wtc():
     else:
         return render_template('change_arduino.html', form=uform, dform = dform,
             sform = sform, gform = gform, iform = iform,
-            diff_form = diff_form, wform = wform, ard=arduino);
-
-@bp.route('/gain_tc', methods=['POST'])
-def gain():
-    '''
-    Configure the new gain for the arduino.
-    '''
-
-    sform = UpdateSetpointForm();
-    uform = UpdateForm();
-    wform = SerialWaitForm()
-    dform = DisconnectForm()
-    gform = UpdateGainForm()
-    iform = UpdateIntegralForm()
-    diff_form = UpdateDifferentialForm()
-
-    id = int(gform.id.data);
-    arduino = TempControl.query.get(id);
-
-    if gform.validate_on_submit():
-        n_gain =  gform.gain.data;
-        arduino.gain = n_gain;
-        db.session.commit();
-        success = arduino.set_gain();
-        if success:
-            flash('We set the gain to {}'.format(n_gain))
-        else:
-            flash('Serial port not open.', 'error')
-        return redirect(url_for('thermocontrol.change_arduino', ard_nr = id))
-    else:
-        return render_template('change_arduino.html', form=uform, dform = dform,
-            cform = cform,  sform = sform, gform = gform, iform = iform,
             diff_form = diff_form, wform = wform, ard=arduino);
 
 @bp.route('/gain_wtc', methods=['POST'])
@@ -358,38 +218,6 @@ def gain_wtc():
             cform = cform,  sform = sform, gform = gform, iform = iform,
             diff_form = diff_form, wform = wform, ard=arduino);
 
-@bp.route('/integral_tc', methods=['POST'])
-def integral():
-    '''
-    Configure the new gain for the arduino.
-    '''
-    sform = UpdateSetpointForm();
-    uform = UpdateForm();
-    wform = SerialWaitForm()
-    dform = DisconnectForm()
-    gform = UpdateGainForm()
-    iform = UpdateIntegralForm()
-    diff_form = UpdateDifferentialForm()
-
-    id = int(iform.id.data);
-    arduino = TempControl.query.get(id);
-
-    if iform.validate_on_submit():
-        n_tau =  iform.tau.data;
-        arduino.integral =  n_tau;
-        db.session.commit();
-        success = arduino.set_integral();
-        if success:
-            flash('We set the integration time  to {} seconds'.format(n_tau))
-        else:
-            flash('Serial port not open.', 'error')
-        return redirect(url_for('thermocontrol.change_arduino', ard_nr = id))
-    else:
-
-        return render_template('change_arduino.html', form=uform, dform = dform,
-        sform = sform, gform = gform, iform = iform,
-            diff_form = diff_form, wform = wform, ard = arduino);
-
 @bp.route('/integral_wtc', methods=['POST'])
 @login_required
 def integral_wtc():
@@ -415,38 +243,6 @@ def integral_wtc():
         sform = sform, gform = gform, iform = iform,
             diff_form = diff_form, wform = wform, ard = arduino);
 
-@bp.route('/diff_tc', methods=['POST'])
-def diff():
-    '''
-    Configure the new gain for the arduino.
-    '''
-
-    sform = UpdateSetpointForm();
-    uform = UpdateForm();
-    wform = SerialWaitForm()
-    dform = DisconnectForm()
-    gform = UpdateGainForm()
-    iform = UpdateIntegralForm()
-    diff_form = UpdateDifferentialForm()
-
-    id = int(diff_form.id.data);
-    arduino = TempControl.query.get(id);
-
-    if diff_form.validate_on_submit():
-        n_tau =  diff_form.tau.data;
-        arduino.diff =  n_tau;
-        db.session.commit();
-        success = arduino.set_differential();
-        if success:
-            flash('We set the differentiation time  to {} seconds'.format(n_tau))
-        else:
-            flash('Serial port not open.', 'error')
-        return redirect(url_for('thermocontrol.change_arduino', ard_nr = id))
-    else:
-        return render_template('change_arduino.html', form=uform, dform = dform,
-            sform = sform, gform = gform, iform = iform,
-            diff_form = diff_form, wform = wform, ard=arduino);
-
 @bp.route('/diff_wtc', methods=['POST'])
 @login_required
 def diff_wtc():
@@ -471,39 +267,3 @@ def diff_wtc():
         return render_template('change_arduino.html', form=uform, dform = dform,
             sform = sform, gform = gform, iform = iform,
             diff_form = diff_form, wform = wform, ard=arduino);
-
-
-
-
-
-@bp.route('/save_tc/<filestring>')
-def file(filestring):
-    '''function to save the values of the hdf5 file. It should have the following structure
-    <ard_nr>+<filename>
-    '''
-    # first let us devide into the right parts
-    parts = filestring.split('+');
-    if not len(parts) == 2:
-        flash('The filestring should be of the form <ard_nr>+<filename>')
-        return redirect(url_for('main.index'))
-
-    filename = parts[1]
-    id = int(parts[0])
-
-    arduino = TempControl.query.get(id);
-
-    if not arduino:
-        flash('Aruduino not installed', 'error')
-        return redirect(url_for('main.index'));
-
-    temp = arduino.get_current_temp_value();
-    param_name = 'tc' + parts[0];
-    with h5py.File(filename, "a") as f:
-        if 'globals' in f.keys():
-            params = f['globals']
-            params.attrs[param_name] = float(temp)
-            flash('Added the temperature {} to the file {}'.format(temp, filename))
-        else:
-            flash('The file {} did not have the global group yet.'.format(filename), 'error')
-
-    return render_template('file.html', file = filename)
